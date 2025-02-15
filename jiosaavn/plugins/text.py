1c0bab52b4211f4b3a3852879b9b45aaa04e3459
Amit <heroku467@gmail.com>
2025-02-15 18:09:19 +0530

    Update text.py

--- jiosaavn/plugins/text.py
+++ jiosaavn/plugins/text.py
@@ -6,42 +6,38 @@ class TEXT:
  
     START_MSG ="""
 **Hello {mention},**
-<blockquote>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴊɪᴏsᴀᴀᴠɴ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ! ᴛʜɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ ᴀʟʟᴏᴡs ʏᴏᴜ ᴛᴏ sᴇᴀʀᴄʜ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴏɴɢs, ᴘʟᴀʏʟɪsᴛs, ᴀʟʙᴜᴍs, ᴀɴᴅ ᴀʀᴛɪsᴛs ᴅɪʀᴇᴄᴛʟʏ ғʀᴏᴍ ᴊɪᴏsᴀᴀᴠɴ.</blockquote>\n\n
-**🔰 Here is the Features :**\n
+<blockquote>Wᴇʟᴄᴏᴍᴇ ᴛᴏ ᴛʜᴇ ᴊɪᴏsᴀᴀᴠɴ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ! ᴛʜɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ ᴀʟʟᴏᴡs ʏᴏᴜ ᴛᴏ sᴇᴀʀᴄʜ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴏɴɢs, ᴘʟᴀʏʟɪsᴛs, ᴀʟʙᴜᴍs, ᴀɴᴅ ᴀʀᴛɪsᴛs ᴅɪʀᴇᴄᴛʟʏ ғʀᴏᴍ ᴊɪᴏsᴀᴀᴠɴ.</blockquote>\n
+**🔰 Here are my Features :**\n
 - 𝘚𝘦𝘢𝘳𝘤𝘩 𝘧𝘰𝘳 𝘴𝘰𝘯𝘨𝘴, 𝘢𝘭𝘣𝘶𝘮𝘴, 𝘱𝘭𝘢𝘺𝘭𝘪𝘴𝘵𝘴, 𝘢𝘯𝘥 𝘢𝘳𝘵𝘪𝘴𝘵𝘴
 - 𝘋𝘰𝘸𝘯𝘭𝘰𝘢𝘥 𝘺𝘰𝘶𝘳 𝘧𝘢𝘷𝘰𝘳𝘪𝘵𝘦 𝘵𝘳𝘢𝘤𝘬𝘴 𝘥𝘪𝘳𝘦𝘤𝘵𝘭𝘺 𝘵𝘰 𝘛𝘦𝘭𝘦𝘨𝘳𝘢𝘮
 - 𝘌𝘹𝘱𝘭𝘰𝘳𝘦 𝘷𝘢𝘳𝘪𝘰𝘶𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦𝘴 𝘵𝘢𝘪𝘭𝘰𝘳𝘦𝘥 𝘵𝘰 𝘦𝘯𝘩𝘢𝘯𝘤𝘦 𝘺𝘰𝘶𝘳 𝘮𝘶𝘴𝘪𝘤 𝘦𝘹𝘱𝘦𝘳𝘪𝘦𝘯𝘤𝘦\n
-**Maintained By: [Anonymous](https://t.me/Ns_AnoNymous)**
+**Maintained By: [𝐻𝑜𝑚𝑜 𝑆𝑎𝑝𝑖𝑒𝑛『❅』](https://t.me/axa_bachha)**
 """
     HELP_MSG ="""
-**It's very simple to use me! 😉**\n\n
+**It's very simple to use me! 😉**\n
 <blockquote>
-1. Start by configuring your preferences using the `/settings` command.\n
-2. Send me the name of a song, playlist, album, or artist you want to search for.\n
-3. I'll handle the rest and provide you with the results!\n\n
+**1. Start by configuring your preferences using the /settings command.**\n
+**2. Send me the name of a song, playlist, album, or artist you want to search for.**\n
+**3. I'll handle the rest and provide you with the results!**\n\n
 **😊 Feel free to explore and enjoy the music!**
 </blockquote>
 """
 
     ABOUT_MSG = """
 <blockquote>
-**🤖 Bot Name:** [🤖 {me.username}](https://t.me/{me.username})
-**📝 Language:** [Python 3](https://www.python.org/)
-**🧰 Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)
-**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)
-**👑 Maintained By:** [RangoZex](https://t.me/RangoZex)
-**📢 Updates Channel:** [NS Bots](https://t.me/NsBotsOfficial)
-**👥 Support Group:** [AMC Support](https://t.me/amcDevSupport)
-**🔗 Source Code:** [GitHub Repository](https://github.com/Ns-AnoNymouS/jiosaavn)
+**⍟───[ MY ᴅᴇᴛᴀɪʟꜱ ]───⍟**
+
+**• Bot Name:** [🤖 {me.username}](https://t.me/{me.username})
+**• Language:** [Python 3](https://www.python.org/)
+**• Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+**• Developer:** [𝐻𝑜𝑚𝑜 𝑆𝑎𝑝𝑖𝑒𝑛『❅』](https://t.me/axa_bachha)
 </blockquote>
 """
 
 ###### FOR REACTION TO ALL COMMAND MESSAGE. ❌ DON'T ADD OR REPLACE ANY EMOJIS. YOU CAN ONLY REMOVE 
     EMOJI_LIST = [
-    "👍", "👎", "❤️", "🔥", "🥰", "👏", "😁", "🤔", "🤯", "😱", "🤬", "😢", "🎉", "🤩", 
-    "🤮", "💩", "🙏", "👌", "🕊", "🤡", "🥱", "🥴", "😍", "🐳", "❤️‍🔥", "🌚", "🌭", "💯", 
-    "🤣", "⚡️", "🍌", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "😭", 
-    "🤓", "👻", "👨‍💻", "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "🎄", 
-    "☃️", "💅", "🤪", "🗿", "🆒", "💘", "🙉", "🦄", "😘", "💊", "🙊", "😎", "👾", "🤷‍♂️", 
-    "🤷", "🤷‍♀️", "😡"
+    "👍", "❤️", "🔥", "🥰", "👏", "😁", "🕊", "😍", "🐳", "❤️‍🔥", "🌚", "💯", "🤣", "⚡️",
+    "😎", "🏆", "💔", "🤨", "😐", "🍓", "🍾", "💋", "🖕", "😈", "😴", "🤓", "👻", "👨‍💻",
+    "👀", "🎃", "🙈", "😇", "😨", "🤝", "✍️", "🤗", "🫡", "🎅", "☃️", "💅", "🤪", "🗿",
+    "🆒", "💘", "🙉", "🦄", "😘", "💊"
 ]
